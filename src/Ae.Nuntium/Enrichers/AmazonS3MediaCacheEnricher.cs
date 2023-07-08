71ae4105a8fae781b2caaeff32a2e894010df855
Alan Edwardes <alan@alanedwardes.com>
2023-07-08 19:42:06 +0100

    Adds AmazonS3MediaCacheEnricher

---
+++ src/Ae.Nuntium/Enrichers/AmazonS3MediaCacheEnricher.cs
@@ -0,0 +1,75 @@
+﻿using Ae.Nuntium.Extractors;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Ae.Nuntium.Enrichers
+{
+    public sealed class AmazonS3MediaCacheEnricher : IExtractedPostEnricher
+    {
+        private readonly ILogger<AmazonS3MediaCacheEnricher> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IAmazonS3 _storage;
+        private readonly Configuration _configuration;
+
+        public sealed class Configuration
+        {
+            public string BucketName { get; set; }
+        }
+
+        public AmazonS3MediaCacheEnricher(ILogger<AmazonS3MediaCacheEnricher> logger, IHttpClientFactory clientFactory, IAmazonS3 storage, Configuration configuration)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+            _storage = storage;
+            _configuration = configuration;
+        }
+
+        public async Task EnrichExtractedPosts(IEnumerable<ExtractedPost> posts, CancellationToken cancellation)
+        {
+            var uriMap = new Dictionary<Uri, Uri>();
+
+            foreach (var mediaUri in posts.SelectMany(x => x.Media).Distinct())
+            {
+                // Do this sync so as not to overload the downstream service
+                uriMap.Add(mediaUri, await CacheMedia(mediaUri, cancellation));
+            }
+
+            foreach (var post in posts)
+            {
+                post.Media = post.Media.Select(x => uriMap[x]).ToHashSet();
+            }
+        }
+
+        private async Task<Uri> CacheMedia(Uri mediaUri, CancellationToken cancellation)
+        {
+            var extension = new FileInfo(mediaUri.AbsoluteUri).Extension;
+
+            var objectKey = Guid.NewGuid() + extension;
+
+            using var httpClient = _clientFactory.CreateClient();
+
+            using var response = await httpClient.GetAsync(mediaUri, cancellation);
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogWarning("Got {StatusCode} when attempting to download {MediaUri}", response.StatusCode, mediaUri);
+                return mediaUri;
+            }
+
+            using var stream = await response.Content.ReadAsStreamAsync(cancellation);
+
+            var putObjectResponse = await _storage.PutObjectAsync(new PutObjectRequest
+            {
+                ContentType = response.Content.Headers.ContentType?.ToString(),
+                InputStream = stream,
+                BucketName = _configuration.BucketName,
+                CannedACL = S3CannedACL.PublicRead,
+                Key = objectKey
+            }, cancellation);
+
+            var preSignedUrl = _storage.GeneratePreSignedURL(_configuration.BucketName, objectKey, DateTime.UtcNow.AddDays(1), null);
+
+            return new UriBuilder(preSignedUrl) { Query = null, Port = -1 }.Uri;
+        }
+    }
+}
---
+++ tests/Ae.Nuntium.Tests/AmazonS3MediaCacheEnricherTests.cs
@@ -0,0 +1,87 @@
+﻿using Ae.Nuntium.Enrichers;
+using Ae.Nuntium.Extractors;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Net;
+using Xunit;
+
+namespace Ae.Nuntium.Tests
+{
+    public sealed class AmazonS3MediaCacheEnricherTests : IDisposable
+    {
+        private readonly MockRepository _repository = new(MockBehavior.Strict);
+
+        public void Dispose() => _repository.VerifyAll();
+
+        [Fact]
+        public async Task TestMediaReplacement()
+        {
+            var mockStorage = _repository.Create<IAmazonS3>();
+
+            var mockStream = new MemoryStream(new byte[] { 1, 2, 3 });
+
+            var media1 = new Uri("https://www.example.com/media.jpeg");
+            var media2 = new Uri("https://www.example.net/media.png");
+
+            var enricher = new AmazonS3MediaCacheEnricher(NullLogger<AmazonS3MediaCacheEnricher>.Instance, new MockHttpClientFactory(request =>
+            {
+                if (request.RequestUri == media1)
+                {
+                    return new HttpResponseMessage
+                    {
+                        Content = new StreamContent(mockStream)
+                        {
+                            Headers = { { "Content-Type", "image/jpeg" } }
+                        }
+                    };
+                }
+
+                if (request.RequestUri == media2)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+                }
+
+                throw new NotImplementedException();
+            }), mockStorage.Object, new AmazonS3MediaCacheEnricher.Configuration
+            {
+                BucketName = "bucketName"
+            });
+
+            mockStorage.Setup(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>(), CancellationToken.None))
+                .Callback<PutObjectRequest, CancellationToken>((request, cancellation) =>
+                {
+                    var ms = new MemoryStream();
+                    request.InputStream.CopyTo(ms);
+                    ms.Position = 0;
+
+                    Assert.Equal("image/jpeg", request.ContentType);
+                    Assert.EndsWith(".jpeg", request.Key);
+                    Assert.Equal(new byte[] { 1, 2, 3 }, ms.ToArray());
+                })
+                .ReturnsAsync(new PutObjectResponse());
+
+            var replacedMedia1 = new Uri("https://www.example.com/replaced");
+
+            mockStorage.Setup(x => x.GeneratePreSignedURL("bucketName", It.IsAny<string>(), It.IsAny<DateTime>(), null))
+                       .Returns(replacedMedia1.ToString());
+
+            var posts = new List<ExtractedPost>
+            {
+                new ExtractedPost(new Uri("https://www.example.com/"))
+                {
+                    Media = new HashSet<Uri> { media1, media2 }
+                }
+            };
+
+            await enricher.EnrichExtractedPosts(posts, CancellationToken.None);
+
+            // This media got replaced
+            Assert.Equal(replacedMedia1, posts[0].Media.First());
+
+            // This errored and didn't
+            Assert.Equal(media2, posts[0].Media.Last());
+        }
+    }
+}
