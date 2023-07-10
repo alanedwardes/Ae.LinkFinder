5e39440470baeb42889e33c3c7b5bcf08208ac8c
Alan Edwardes <alan@alanedwardes.com>
2023-07-10 22:49:12 +0100

    Even more logging for HttpClientExtensions.

--- src/Ae.Nuntium/Services/HttpClientExtensions.cs
+++ src/Ae.Nuntium/Services/HttpClientExtensions.cs
@@ -9,8 +9,11 @@ namespace Ae.Nuntium.Services
             var response = await requestTask;
             if (!response.IsSuccessStatusCode)
             {
+                var requestAsString = await (response.RequestMessage?.Content?.ReadAsStringAsync() ?? Task.FromResult<string>(null));
                 var responseAsString = await response.Content.ReadAsStringAsync();
-                throw new HttpRequestException($"Got status code {response.StatusCode} from {response.RequestMessage?.RequestUri}. Response: {responseAsString.Truncate(2048)}");
+                throw new HttpRequestException($"Got status code {response.StatusCode} from {response.RequestMessage?.RequestUri}." +
+                    $"\nRequest: {requestAsString.Truncate(2048)}" +
+                    $"\nResponse: {responseAsString.Truncate(2048)}");
             }
 
             return response;
