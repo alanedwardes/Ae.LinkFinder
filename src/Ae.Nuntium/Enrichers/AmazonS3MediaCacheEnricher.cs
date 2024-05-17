7eaedde51a1f87db45b5f32d01ae7b7a70754ed3
Alan Edwardes <alan@alanedwardes.com>
2024-05-17 22:41:36 +0100

    Adds ability to specify a URI format and key format for S3 uploads.

--- src/Ae.Nuntium/Enrichers/AmazonS3MediaCacheEnricher.cs
+++ src/Ae.Nuntium/Enrichers/AmazonS3MediaCacheEnricher.cs
@@ -15,7 +15,25 @@ namespace Ae.Nuntium.Enrichers
 
         public sealed class Configuration
         {
+            /// <summary>
+            /// The name of the S3 bucket.
+            /// </summary>
             public string BucketName { get; set; }
+            /// <summary>
+            /// The canned ACL to use when uploading the object.
+            /// </summary>
+            public string CannedAcl { get; set; } = "NoACL";
+            /// <summary>
+            /// Format string to use when constructing the object key
+            /// {0} is a new guid
+            /// </summary>
+            public string KeyFormat { get; set; } = "{0}";
+            /// <summary>
+            /// Format string to use when constructing the final URI
+            /// {0} is the full URL to the object in S3
+            /// {1} is the object key
+            /// </summary>
+            public string UriFormat { get; set; } = "{0}";
         }
 
         public AmazonS3MediaCacheEnricher(ILogger<AmazonS3MediaCacheEnricher> logger, IHttpClientFactory clientFactory, IAmazonS3 storage, Configuration configuration)
@@ -80,7 +98,7 @@ namespace Ae.Nuntium.Enrichers
 
         private async Task<Uri> CacheMedia(Uri mediaUri, CancellationToken cancellation)
         {
-            var objectKey = Guid.NewGuid().ToString();
+            var objectKey = string.Format(_configuration.KeyFormat, Guid.NewGuid().ToString());
 
             using var httpClient = _clientFactory.CreateClient("GZIP_CLIENT");
             using var response = await httpClient.GetAsync(mediaUri, cancellation);
@@ -91,15 +109,16 @@ namespace Ae.Nuntium.Enrichers
                 ContentType = response.Content.Headers.ContentType?.ToString(),
                 InputStream = stream,
                 BucketName = _configuration.BucketName,
-                CannedACL = S3CannedACL.PublicRead,
+                CannedACL = S3CannedACL.FindValue(_configuration.CannedAcl),
                 Key = objectKey
             }, cancellation);
 
             // Generate a signed URL to easily resolve a full URL for the object,
             // but strip the crypto from the query parameters
             var preSignedUrl = _storage.GeneratePreSignedURL(_configuration.BucketName, objectKey, DateTime.UtcNow.AddDays(1), null);
+            var objectUri = new Uri(preSignedUrl).RemoveFragmentAndQueryString();
 
-            return new Uri(preSignedUrl).RemoveFragmentAndQueryString();
+            return new Uri(string.Format(_configuration.UriFormat, objectUri, objectKey));
         }
     }
 }
